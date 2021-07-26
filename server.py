2df911174894eca2992b2ac76c2f882726f659b1
Didier Irias <didier.irias@gmail.com>
2021-07-25 18:07:18 -0600

    Project completed

--- server.py
+++ server.py
@@ -7,7 +7,9 @@ from types import MethodDescriptorType
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 import linked_list
-import hash_table
+import hash_table, custom_q, stack
+import random
+import binary_search_tree
 
 #Create all SQLAlchemy functions inside the application
 from flask_sqlalchemy import SQLAlchemy
@@ -154,14 +156,74 @@ def create_blog_post(user_id):
     db.session.commit()
     return jsonify({"message": "new blog post created"}), 200
 
-@app.route("/user/<user_id>", methods=["GET"])
-def get_all_blog_posts(user_id):
-    pass
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
-def get_one_blog_post(blog_post_id):
-    pass
-@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
-def delete_blog_post(blog_post_id):
-    pass
+def get_all_blog_posts(blog_post_id):
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+
+    bst = binary_search_tree.BinarySearchTree()
+
+    for post in blog_posts:
+        bst.insert({
+            "id" : post.id,
+            "title" : post.title,
+            "body" : post.body,
+            "user_id" : post.user_id,
+        })
+
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message": "post not found"})
+
+    return jsonify(post)
+    
+@app.route("/blog_post/numeric_body", methods=["GET"])
+def get_numeric_post_bodies():
+    blog_posts = BlogPost.query.all()
+
+    q = custom_q.Queue()
+
+    for post in blog_posts:
+        q.enqueue(post)
+
+    return_list = []
+
+    for _ in range(len(blog_posts)):
+        post = q.dequeue()
+        numeric_body = 0
+        for char in post.data.body:
+            numeric_body += ord(char)
+
+        post.data.body = numeric_body
+
+        return_list.append(
+            {
+                "id": post.data.id,
+                "title" : post.data.title,
+                "body" : post.data.body,
+                "user_id" : post.data.user_id,
+            }
+        )
+
+    return jsonify(return_list)
+
+@app.route("/blog_post/delete_last_10", methods=["DELETE"])
+def delete_last_10():
+
+    blog_posts = BlogPost.query.all()
+
+    s = stack.Stack()
+
+    for post in blog_posts:
+        s.push(post)
+
+    for _ in range(10):
+        post_to_delete = s.pop()
+        db.session.delete(post_to_delete.data)
+        db.session.commit()
+
+    return jsonify({"message" : "success"})
+
 if __name__ == "__main__":
     app.run(debug=True)
